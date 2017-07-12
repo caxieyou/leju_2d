b4209a4443feac03085e18a32f36c2d7f7f4f476
caxieyou <248500947@qq.com>
2017-07-13 01:23:56 +0800

    MOD-develop #refactory stash 70% done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
-var TYPE = {};
-TYPE.CIRCLE     = 0;
-TYPE.RECT       = 1;
-TYPE.LINEPATH   = 2;
+
 
 var canvas = null;
 var type = TYPE.RECT;
@@ -28,150 +25,14 @@ function onClear() {
 };
 
 function onOutput() {
+    //get the objects on the page
     var objs = canvas.getObjects();
-    var index = 0;
-    var resPolygonArray = [];
-    var resPolygonPartsArray = [];
-    var resLineArray = [];
     
-    for(var i = 0; i < objs.length; i++) {
-        if (objs[i].type === "path") {
-            resLineArray["line_" +index] = getPath(objs[i]);
-        } else{
-            resPolygonArray["polygon_" +index] = getPath(objs[i]);
-        }
-        index++;
-    }
-
-    for(var line in resLineArray) {
-        //get all the line segments
-        var segements = getSegements(resLineArray[line]);
-        for (var rect in resPolygonArray) {
-            //calculate the intersection between line and rect
-            for(var i = 0; i < segements.length; i++){
-                var intersect = null; 
-                intersect = lineIntersectWithRect(segements[i], resPolygonArray[rect]);
-                
-                if(intersect) {
-                    resPolygonPartsArray["polygon_" + index] = intersect[0];
-                    index++;
-                    resPolygonPartsArray["polygon_" + index] = intersect[1];
-                    index++;
-                } else {
-                    if(!resPolygonPartsArray[rect]) {
-                        resPolygonPartsArray[rect] = resPolygonArray[rect];
-                    }
-                }
-            }
-            
-        }
-    }
-
-    var cpr = new ClipperLib.Clipper();
-    cpr.StrictlySimple = true;
-    var solution_intersect = new ClipperLib.Paths();
-    var solution_diff = new ClipperLib.Paths();
-    var solution_intersect_reverse = new ClipperLib.Paths();
-    var solution_diff_reverse = new ClipperLib.Paths();
-    
-    var breakOut = false;
-
-    while(true && isNotSingle(resPolygonPartsArray)) {
-        for (var key0 in resPolygonPartsArray) {
-            for (var key1 in resPolygonPartsArray) {
-                breakOut = false;
-                
-                //skip if same
-                if (key0 === key1) {
-                    continue;
-                }
-                
-                //calculate positive
-                cpr.Clear();
-                if(resPolygonPartsArray[key0][0] instanceof Array) {
-                    cpr.AddPaths(resPolygonPartsArray[key0], ClipperLib.PolyType.ptSubject, true);  // true means closed path
-                }else {
-                    cpr.AddPath(resPolygonPartsArray[key0], ClipperLib.PolyType.ptSubject, true);  // true means closed path
-                }
-                
-                if(resPolygonPartsArray[key1][0] instanceof Array) {
-                    cpr.AddPaths(resPolygonPartsArray[key1], ClipperLib.PolyType.ptClip, true);
-                }else {
-                    cpr.AddPath(resPolygonPartsArray[key1], ClipperLib.PolyType.ptClip, true);
-                }
-                
-                cpr.Execute(ClipperLib.ClipType.ctIntersection, solution_intersect, ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
-                cpr.Execute(ClipperLib.ClipType.ctDifference,   solution_diff,      ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
-                
-                //calculate with reverse
-                cpr.Clear();
-                if(resPolygonPartsArray[key1][0] instanceof Array) {
-                    cpr.AddPaths(resPolygonPartsArray[key1], ClipperLib.PolyType.ptSubject, true);  // true means closed path
-                }else {
-                    cpr.AddPath(resPolygonPartsArray[key1], ClipperLib.PolyType.ptSubject, true);  // true means closed path
-                }
-                
-                if(resPolygonPartsArray[key0][0] instanceof Array) {
-                    cpr.AddPaths(resPolygonPartsArray[key0], ClipperLib.PolyType.ptClip, true);
-                }else {
-                    cpr.AddPath(resPolygonPartsArray[key0], ClipperLib.PolyType.ptClip, true);
-                }
-                cpr.Execute(ClipperLib.ClipType.ctIntersection, solution_intersect_reverse, ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
-                cpr.Execute(ClipperLib.ClipType.ctDifference,   solution_diff_reverse,      ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
-                
-                //no intersection
-                if (solution_intersect.length === 0) {
-                    continue;
-                
-                //one includes the other
-                } else if(solution_diff.length === 0 || solution_diff_reverse.length === 0){
-                    
-                    delete resPolygonPartsArray[key0];
-                    delete resPolygonPartsArray[key1];
-                    
-                    resPolygonPartsArray["polygon_" +index] = solution_diff.length === 0 ? addPathXOR(solution_diff_reverse) : addPathXOR(solution_diff);
-                    index++;
-                    
-                    breakOut = true;
-                    break;
-                } else {
-                    delete resPolygonPartsArray[key0];
-                    delete resPolygonPartsArray[key1];
-                    
-                    for(var i = 0; i < solution_intersect.length; i++) {
-                        resPolygonPartsArray["polygon_" +index] = addPath(solution_intersect[i]);
-                        index++;
-                    }
-                    
-                    for(var i = 0; i < solution_diff.length; i++) {
-                        resPolygonPartsArray["polygon_" +index] = addPath(solution_diff[i]);
-                        index++;
-                    }
-                    
-                    for(var i = 0; i < solution_diff_reverse.length; i++) {
-                        resPolygonPartsArray["polygon_" +index] = addPath(solution_diff_reverse[i]);
-                        index++;
-                    }
-                    breakOut = true;
-                    break;
-                }
-            }
-            if (breakOut) {
-                break;
-            }
-        }
-        if(!breakOut) {
-            break;
-        }
-    }
-
-    console.log(resPolygonPartsArray);
-    console.log(resLineArray);
-    
+    //do the caculation
+    process(objs);
 };
 
 function onMouseDown(options) {
-    
     switch (type) {
         case TYPE.CIRCLE:
             canvas.add(createCircle(options));
@@ -209,16 +70,16 @@ function onMouseMove(options) {
             break;
             
             case TYPE.LINEPATH:
-                
+                //do nothing
             break;
         }
     }
-    
     
     canvas.renderAll();
 };
 
 function main() {
+    var line = new fabric.Path('M 0 0');
     canvas = new fabric.Canvas('canvas');
     
     canvas.on('mouse:down', onMouseDown);
--- src/utility.js
+++ src/utility.js
@@ -1,148 +1,201 @@
+var TYPE = {};
+TYPE.CIRCLE     = 0;
+TYPE.RECT       = 1;
+TYPE.LINEPATH   = 2;
+
 function createCircle(options) {
     var circle = new fabric.Circle({
-        radius: 0, left: 0, top: 0, fill : 'transparent', stroke: 'black', selectable: false
-    });
-    circle.centerX = options.e.offsetX;
-    circle.centerY = options.e.offsetY;
+                            radius      : 0, 
+                            left        : 0, 
+                            top         : 0, 
+                            fill        : 'transparent', 
+                            stroke      : 'black', 
+                            selectable  : false
+                        });
+                        
+    circle.center = new MyPoint(options.e.offsetX, options.e.offsetY);
+    
     return circle;
 }
 
 function updateCircle(circle, options) {
-    var centerX = circle.centerX;
-    var centerY = circle.centerY;
-    
-    var right = options.e.offsetX;
-    var buttom = options.e.offsetY;
-    
-    var radius = Math.sqrt((right - centerX) * ((right - centerX)) + (centerY - buttom) * (centerY - buttom));
-    
+    var corner = new MyPoint(options.e.offsetX, options.e.offsetY);
+
     circle.set({
-        "radius": radius,
-        "left": centerX - radius,
-        "top": centerY - radius
+        "radius": MyMath.getLength(circle.center, corner),
+        "left"  : circle.center.X - radius,
+        "top"   : circle.center.Y - radius
     });
 }
 
 function createRect(options) {
     var rect = new fabric.Rect({
-        left: 0, 
-        top: 0, 
-        width : 0,
-        height : 0,
-        fill : 'transparent', 
-        stroke: 'black',
-        selectable: false
+        left        : 0, 
+        top         : 0, 
+        width       : 0,
+        height      : 0,
+        fill        : 'transparent', 
+        stroke      : 'black',
+        selectable  : false
     });
 
-    rect.startX = options.e.offsetX;
-    rect.startY = options.e.offsetY;
-    
+    rect.start = new MyPoint(options.e.offsetX, options.e.offsetY);
     return rect;
 }
 
 function updateRect(rect, options) {
-    var _left = rect.startX;
-    var _top = rect.startY;
-    
-    var _right = options.e.offsetX;
-    var _bottom = options.e.offsetY;
+    var corner = new MyPoint(options.e.offsetX, options.e.offsetY);
     
     rect.set({
-        left : Math.min(_left, _right),
-        top:   Math.min(_top, _bottom),
-        width: Math.abs(_left - _right),
-        height: Math.abs(_top - _bottom)
+        left    : Math.min(rect.start.X, corner.X),
+        top     : Math.min(rect.start.Y, corner.Y),
+        width   : Math.abs(rect.start.X - corner.X),
+        height  : Math.abs(rect.start.Y - corner.Y)
     });
 }
 
 function createLinePath() {
-    var path = new fabric.Path('M 0 0');
-    path.set({ 
+    var line = new fabric.Path('M 0 0');
+    line.set({ 
                 fill: 'transparent', 
                 stroke: 'black',
                 selectable: false
-    });
+            });
 
-    return path;
+    return line;
 }
 
-function updateLinePath(path, options){
-    if(path.path.length === 1 && path.isInit === undefined) {
-        path.set({
-            left: options.e.offsetX,
-            top: options.e.offsetY
-        });
-        path.isInit = true;
-        path.startX = options.e.offsetX;
-        path.startY = options.e.offsetY;
+function updateLinePath(line, options){
+    if(line.path.length === 1 && line.isInit === undefined) {
+        line.set({
+                    left: options.e.offsetX,
+                    top : options.e.offsetY
+                });
+        line.isInit = true;
+        line.start = new MyPoint(options.e.offsetX, options.e.offsetY);
     } else {
         var commandArray = [];
         commandArray[0] = 'L';
-        commandArray[1] = options.e.offsetX - path.startX;
-        commandArray[2] = options.e.offsetY - path.startY;
-        path.path[path.path.length] = commandArray;
-        _updateDims(path);
+        commandArray[1] = options.e.offsetX - line.start.X;
+        commandArray[2] = options.e.offsetY - line.start.Y;
+        line.path[line.path.length] = commandArray;
+        _updateLine(line);
     }
 }
 
-function getKeyInfo(obj) {
-    var res = {};
-    if(obj.type === "rect") {
-        res.x = obj.startX;
-        res.y = obj.startY;
-        res.width = obj.getWidth();
-        res.height = obj.getHeight();
-        res.type = "rect";
-        res.path = [{X: res.x, Y: res.y}, {X: res.x + res.width, Y: res.y}, {X: res.x + res.width, Y: res.y + res.height}, {X: res.x, Y: res.y + res.height}];
-    } else if(obj.type === "circle") {
-        res.x = obj.centerX;
-        res.y = obj.centerY;
-        res.radius = obj.radius;
-        res.type = "circle";
-    } else if(obj.type === "path") {
-        res.path = [];
-        for (var i = 0; i < obj.path.length; i++) {
-            if(obj.path[i][0] === 'M' || obj.path[i][0] === 'L'){
-                res.path.push({X: obj.path[i][1] + obj.startX, Y: obj.path[i][2] + obj.startY});
-            }
-        }
-        res.type = "path";
-        
+function _updateLine(line) {
+    var dims = line._parseDimensions(),
+        prevDims = line.prevDims || {},
+        leftDiff = dims.left - (prevDims.left || 0),
+        topDiff = dims.top - (prevDims.top || 0);
+
+    line.setWidth(dims.width);
+    line.setHeight(dims.height);
+    
+    if (dims.left < 0) {
+        line.pathOffset.x = line.width/2 + dims.left;
+        line.left = line.left + leftDiff;
+    } else {
+        line.pathOffset.x = line.width/2;
     }
-    return res;
+    
+    if (dims.top < 0) {
+        line.pathOffset.y = line.height/2 + dims.top;
+        line.top = line.top + topDiff;
+    } else {
+         line.pathOffset.y = line.height/2;   
+    }
+    
+    line.prevDims = dims;
+    line.setCoords();
 }
 
-function isNotSingle(paths) {
-    var sum = 0;
-    for(var x in paths) {
-        sum++;
-        if(sum > 1) {
+function _isNotSingle(paths) {
+    var num = 0;
+    for(var path in paths) {
+        num++;
+        if(num > 1) {
             return true;
         }
     }
     return false;
 }
 
-function getSegements(path) {
+function _getPath(obj) {
+    if(obj.type === "rect") {
+        return [{X: obj.start.X,                    Y: obj.start.Y                  }, 
+                {X: obj.start.X + obj.getWidth(),   Y: obj.start.Y                  }, 
+                {X: obj.start.X + obj.getWidth(),   Y: obj.start.Y + obj.getHeight()}, 
+                {X: obj.start.X,                    Y: obj.start.Y + obj.getHeight()}];
+    } else if(obj.type === "circle") {
+        //circle is not ready
+    } else if(obj.type === "path") {
+        var path = [];
+        for (var i = 0; i < obj.path.length; i++) {
+            if(obj.path[i][0] === 'M' || obj.path[i][0] === 'L'){
+                path.push(
+                          { X: obj.path[i][1] + obj.start.X, 
+                            Y: obj.path[i][2] + obj.start.Y}
+                         );
+            }
+        }
+        return path;
+    }
+}
+
+function _copyPath(path) {
     var res = [];
-    for(var i = 0; i < path.length - 1; i++) {
-        res.push({point0: path[i], point1: path[i+1]});
+    if (path[0] instanceof Array) {
+        for(var i = 0; i < path.length; i++) {
+            var tmp = [];
+            for(var j = 0; j < path[i].length; j++) {
+                var point = new MyPoint(path[i][j]);
+                tmp.push(point);
+            }
+            res[i] = tmp;
+        }
+    } else {
+        for(var i = 0; i < path.length; i++) {
+            var point = new MyPoint(path[i]);
+            res.push({X: path[i].X, Y: path[i].Y});
+        }
     }
     return res;
 }
 
-function lineIntersectWithRect(segement, rect) {
+//get All the segments
+function _getSegements(path, type) {
+    var res = [];
+    switch (type) {
+        case TYPE.LINEPATH:
+        {
+            for(var i = 0; i < path.length - 1; i++) {
+                var segment = new MySegment(path[i], path[i+1]);
+                res.push(segment);
+            }
+        }
+        break;
+        
+        case TYPE.RECT:
+        {
+            for(var i = 0; i < path.length; i++) {
+                var segment = new MySegment(path[i], path[(i+1) % path.length]);
+                res.push(segment);
+            }
+        }
+        break;
+    }
+    return res;
+}
+
+function _intersectSegmentRect(segement, rect) {
     var res = [];
     var intersect = null;
-    var rectLine = [];
-    rectLine[0] = {point0 : rect[0], point1 : rect[1]};
-    rectLine[1] = {point0 : rect[1], point1 : rect[2]};
-    rectLine[2] = {point0 : rect[2], point1 : rect[3]};
-    rectLine[3] = {point0 : rect[3], point1 : rect[0]};
+    var rectSegments = _getSegements(rect, TYPE.RECT);
     
     var record = [];
-    for(var i = 0; i < 4; i++) {
-        var inter = _segmentsIntr(segement.point0, segement.point1, rectLine[i].point0, rectLine[i].point1);
+    for(var i = 0; i < rectSegments.length; i++) {
+        var inter = MyMath.lineSegmentsIntersect(segement, rectSegments[i]);
         if(inter) {
             res.push(inter);
             record.push(i);
@@ -229,101 +282,148 @@ function lineIntersectWithRect(segement, rect) {
     return intersect;
 }
 
-function getPath(obj) {
-    if(obj.type === "rect") {
-        var res = {};
-        res.x = obj.startX;
-        res.y = obj.startY;
-        res.width = obj.getWidth();
-        res.height = obj.getHeight();
-        return [{X: res.x, Y: res.y}, {X: res.x + res.width, Y: res.y}, {X: res.x + res.width, Y: res.y + res.height}, {X: res.x, Y: res.y + res.height}];
-    } else if(obj.type === "circle") {
-        //circle is not ready
-        //res.x = obj.centerX;
-        //res.y = obj.centerY;
-        //res.radius = obj.radius;
-        //res.type = "circle";
-        
-    } else if(obj.type === "path") {
-        var path = [];
-        for (var i = 0; i < obj.path.length; i++) {
-            if(obj.path[i][0] === 'M' || obj.path[i][0] === 'L'){
-                path.push({X: obj.path[i][1] + obj.startX, Y: obj.path[i][2] + obj.startY});
+function process(objs) {
+    var index = 0;
+    var resPolygonArray = [];
+    var resPolygonPartsArray = [];
+    var resLineArray = [];
+    
+    for(var i = 0; i < objs.length; i++) {
+        if (objs[i].type === "path") {
+            resLineArray["line_" +index] = _getPath(objs[i]);
+        } else{
+            resPolygonArray["polygon_" +index] = _getPath(objs[i]);
+        }
+        index++;
+    }
+
+    if (_isNotSingle(resLineArray)) {
+        for(var line in resLineArray) {
+            //get all the line segments
+            var segements = _getSegements(resLineArray[line], TYPE.LINEPATH);
+            for (var rect in resPolygonArray) {
+                //calculate the intersection between line and rect
+                for(var i = 0; i < segements.length; i++){
+                    var intersect = null; 
+                    intersect = _intersectSegmentRect(segements[i], resPolygonArray[rect]);
+                    
+                    if(intersect) {
+                        resPolygonPartsArray["polygon_" + index] = intersect[0];
+                        index++;
+                        resPolygonPartsArray["polygon_" + index] = intersect[1];
+                        index++;
+                    } else {
+                        if(!resPolygonPartsArray[rect]) {
+                            resPolygonPartsArray[rect] = resPolygonArray[rect];
+                        }
+                    }
+                }
+                
             }
         }
-        return path;
+    } else {
+        resPolygonPartsArray = resPolygonArray;
     }
-}
+    
+    
+    var cpr = new ClipperLib.Clipper();
+    cpr.StrictlySimple = true;
+    var solution_intersect = new ClipperLib.Paths();
+    var solution_diff = new ClipperLib.Paths();
+    var solution_intersect_reverse = new ClipperLib.Paths();
+    var solution_diff_reverse = new ClipperLib.Paths();
+    
+    var breakOut = false;
 
-function addPath(path) {
-    var res = [];
-    for(var i = 0; i < path.length; i++) {
-        res.push({X: path[i].X, Y: path[i].Y});
-    }
-    return res;
-}
-
-function addPathXOR(xor) {
-    var res = [];
-    for(var i = 0; i < xor.length; i++) {
-        var tmp = [];
-        for(var j = 0; j < xor[i].length; j++) {
-            tmp.push({X: xor[i][j].X, Y: xor[i][j].Y});
+    while(true && _isNotSingle(resPolygonPartsArray)) {
+        for (var key0 in resPolygonPartsArray) {
+            for (var key1 in resPolygonPartsArray) {
+                breakOut = false;
+                
+                //skip if same
+                if (key0 === key1) {
+                    continue;
+                }
+                
+                //calculate positive
+                cpr.Clear();
+                if(resPolygonPartsArray[key0][0] instanceof Array) {
+                    cpr.AddPaths(resPolygonPartsArray[key0], ClipperLib.PolyType.ptSubject, true);  // true means closed path
+                }else {
+                    cpr.AddPath(resPolygonPartsArray[key0], ClipperLib.PolyType.ptSubject, true);  // true means closed path
+                }
+                
+                if(resPolygonPartsArray[key1][0] instanceof Array) {
+                    cpr.AddPaths(resPolygonPartsArray[key1], ClipperLib.PolyType.ptClip, true);
+                }else {
+                    cpr.AddPath(resPolygonPartsArray[key1], ClipperLib.PolyType.ptClip, true);
+                }
+                
+                cpr.Execute(ClipperLib.ClipType.ctIntersection, solution_intersect, ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
+                cpr.Execute(ClipperLib.ClipType.ctDifference,   solution_diff,      ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
+                
+                //calculate with reverse
+                cpr.Clear();
+                if(resPolygonPartsArray[key1][0] instanceof Array) {
+                    cpr.AddPaths(resPolygonPartsArray[key1], ClipperLib.PolyType.ptSubject, true);  // true means closed path
+                }else {
+                    cpr.AddPath(resPolygonPartsArray[key1], ClipperLib.PolyType.ptSubject, true);  // true means closed path
+                }
+                
+                if(resPolygonPartsArray[key0][0] instanceof Array) {
+                    cpr.AddPaths(resPolygonPartsArray[key0], ClipperLib.PolyType.ptClip, true);
+                }else {
+                    cpr.AddPath(resPolygonPartsArray[key0], ClipperLib.PolyType.ptClip, true);
+                }
+                cpr.Execute(ClipperLib.ClipType.ctIntersection, solution_intersect_reverse, ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
+                cpr.Execute(ClipperLib.ClipType.ctDifference,   solution_diff_reverse,      ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
+                
+                //no intersection
+                if (solution_intersect.length === 0) {
+                    continue;
+                
+                //one includes the other
+                } else if(solution_diff.length === 0 || solution_diff_reverse.length === 0){
+                    
+                    delete resPolygonPartsArray[key0];
+                    delete resPolygonPartsArray[key1];
+                    
+                    resPolygonPartsArray["polygon_" +index] = solution_diff.length === 0 ? _copyPath(solution_diff_reverse) : _copyPath(solution_diff);
+                    index++;
+                    
+                    breakOut = true;
+                    break;
+                } else {
+                    delete resPolygonPartsArray[key0];
+                    delete resPolygonPartsArray[key1];
+                    
+                    for(var i = 0; i < solution_intersect.length; i++) {
+                        resPolygonPartsArray["polygon_" +index] = _copyPath(solution_intersect[i]);
+                        index++;
+                    }
+                    
+                    for(var i = 0; i < solution_diff.length; i++) {
+                        resPolygonPartsArray["polygon_" +index] = _copyPath(solution_diff[i]);
+                        index++;
+                    }
+                    
+                    for(var i = 0; i < solution_diff_reverse.length; i++) {
+                        resPolygonPartsArray["polygon_" +index] = _copyPath(solution_diff_reverse[i]);
+                        index++;
+                    }
+                    breakOut = true;
+                    break;
+                }
+            }
+            if (breakOut) {
+                break;
+            }
         }
-        res[i] = tmp;
+        if(!breakOut) {
+            break;
+        }
     }
-    return res;
-}
 
-function _segmentsIntr(a, b, c, d){  
-    //twice of the triangle area  
-    var area_abc = (a.X - c.X) * (b.Y - c.Y) - (a.Y - c.Y) * (b.X - c.X);  
-  
-    //twice of the triangle area  
-    var area_abd = (a.X - d.X) * (b.Y - d.Y) - (a.Y - d.Y) * (b.X - d.X);   
-  
-    //if the sign of areas are same which means they are on the same side  
-    if ( area_abc*area_abd>=0 ) {  
-        return null;  
-    }  
-    var area_cda = (c.X - a.X) * (d.Y - a.Y) - (c.Y - a.Y) * (d.X - a.X);  
-    
-    var area_cdb = area_cda + area_abc - area_abd ;  
-    if (  area_cda * area_cdb >= 0 ) {  
-        return null;  
-    }  
-    
-    //calculate the intersect point
-    var t = area_cda / ( area_abd- area_abc );  
-    var dx= t*(b.X - a.X),  
-        dy= t*(b.Y - a.Y);  
-    return { X: Math.round(a.X + dx) , Y: Math.round(a.Y + dy) };  
-  
-}  
-
-function _updateDims(path) {
-    var dims = path._parseDimensions(),
-        prevDims = path.prevDims || {},
-        leftDiff = dims.left - (prevDims.left || 0),
-        topDiff = dims.top - (prevDims.top || 0);
-
-    path.setWidth(dims.width);
-    path.setHeight(dims.height);
-    
-    if (dims.left < 0) {
-        path.pathOffset.x = path.width/2 + dims.left;
-        path.left = path.left + leftDiff;
-    } else {
-        path.pathOffset.x = path.width/2;
-    }
-    
-    if (dims.top < 0) {
-        path.pathOffset.y = path.height/2 + dims.top;
-        path.top = path.top + topDiff;
-    } else {
-         path.pathOffset.y = path.height/2;   
-    }
-    
-    path.prevDims = dims;
-    path.setCoords();
+    console.log(resPolygonPartsArray);
+    console.log(resLineArray);
 }
